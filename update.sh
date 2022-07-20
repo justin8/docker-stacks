bd54892878cb8ce4ba59079b0fc2055ae5bbbcdb
Justin Dray <justin@dray.be>
2022-07-20 21:11:32 +1000

    Add update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Usage: ./update.sh APP_NAME [--no-restart]
+#
+
+app="$1"
+no_restart="$2"
+
+if [[ -z $app ]]; then
+	echo "You must provide the name of an app to update"
+fi
+
+cd "$(readlink -f "$(dirname "$0")")/$app"
+
+docker-compose pull
+docker-compose build
+
+if [[ -z $no_restart ]]; then
+	docker-compose restart
+fi
